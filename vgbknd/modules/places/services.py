1d7e56271e7cba211841b73531eb51a4b1e7be53
Sebastian <jsebastpz@gmail.com>
2021-06-15 15:27:44 -0500

    vguide-30: nearby places

--- vgbknd/modules/places/services.py
+++ vgbknd/modules/places/services.py
@@ -16,6 +16,9 @@ class PlaceService:
         dlon = lon2 - lon1
         dlat = lat2 - lat1
 
+        print('dlon: ', dlon)
+        print('dlat: ', dlat)
+
         a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
