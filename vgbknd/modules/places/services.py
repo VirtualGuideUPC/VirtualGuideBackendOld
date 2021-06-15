eb2288e5ad9489a36d5067cbc559e1c980fd47c1
Sebastian <jsebastpz@gmail.com>
2021-06-15 15:04:25 -0500

    vguide-25: nearby places

--- vgbknd/modules/places/services.py
+++ vgbknd/modules/places/services.py
@@ -28,8 +28,8 @@ class PlaceService:
         list = []
         print('entre')
         for tp in tplist:
-            d = self.distance(self.lat, self.lon, tp.latitude, tp.longitude)
             print ('tp latitude: ', tp.latitude)
+            d = self.distance(self.lat, self.lon, tp.latitude, tp.longitude)
             if d < 3:
                 list.append(object)
 
