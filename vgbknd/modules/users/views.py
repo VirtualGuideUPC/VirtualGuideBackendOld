582811fed5131c0d6bde3a33935163e5d38328cc
Sebastian <jsebastpz@gmail.com>
2021-06-15 18:38:24 -0500

    vguide-47: login correction

--- vgbknd/modules/users/views.py
+++ vgbknd/modules/users/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import AccountSerializer
 from .models import *
-from PyJWT import jwt   
+import jwt   
 import datetime
 
 # Create your views here.
