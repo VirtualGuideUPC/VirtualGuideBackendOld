51179aa8f84ec83546c7914a289095fbc54c262c
Sebastian <jsebastpz@gmail.com>
2021-06-20 11:08:40 -0500

    vguide-76: place detail

--- vgbknd/modules/places/views.py
+++ vgbknd/modules/places/views.py
@@ -48,7 +48,7 @@ class TouristicPlaceById(APIView):
         tppictures = PictureTouristicPlace.objects.filter(touristic_place=pk)
         picturesSerializer = PictureTouristicPlaceSerializer(tppictures, many=True)
         response = Response()
-        print("pictures", tppictures)
+        print("pictures", picturesSerializer)
         response.data = {
             'pictures': picturesSerializer,
             'name': touristicPlace.name,
