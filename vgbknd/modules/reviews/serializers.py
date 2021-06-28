75162eb357594dea6ab6f8331bae8f57d0fd86fd
Sebastian <jsebastpz@gmail.com>
2021-06-28 02:32:09 -0500

    vguide-92: place detail

--- vgbknd/modules/reviews/serializers.py
+++ vgbknd/modules/reviews/serializers.py
@@ -15,11 +15,15 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class ReviewTpSerializer(serializers.ModelSerializer):
+    user_id = serializers.SerializerMethodField('get_user_id')
     user_name = serializers.SerializerMethodField('get_user_name')
 
     class Meta:
         model = Review
-        fields = ['user_name', 'date', 'comment', 'ranking'] 
+        fields = ['user_id', 'user_name', 'date', 'comment', 'ranking'] 
+
+    def get_user_id(self, obj):
+        return obj.user.account_id
 
     def get_user_name(self, obj):
         return obj.user.name
