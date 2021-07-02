a4aac6c4358c010489f91dd438be64ec12024ba7
Sebastian <jsebastpz@gmail.com>
2021-07-01 23:31:07 -0500

    vguide-133: comments and ranking

--- vgbknd/modules/places/serializers.py
+++ vgbknd/modules/places/serializers.py
@@ -64,8 +64,9 @@ class NearbyPlaceSerializer(serializers.ModelSerializer):
     def get_picture(self, obj):
         tp_id = obj.touristicplace_id
         tppicture = PictureTouristicPlace.objects.filter(touristic_place=tp_id).values('url').first()
+        new = tppicture.url
         tpicture = PictureTpUrlSerializer(tppicture)
         print("Tp: ", tpicture)
-        return str(tppicture)
+        return str(new)
     
  
