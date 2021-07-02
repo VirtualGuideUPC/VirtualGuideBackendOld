f0d03126492887d26c085df92e7c3ea5f236496c
Sebastian <jsebastpz@gmail.com>
2021-07-02 10:22:05 -0500

    vguide-148: update preferences

--- vgbknd/modules/users/urls.py
+++ vgbknd/modules/users/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.urls import path
-from .views import AddFavourite, AddCategoryPreference, AddTypePlacePreference, ListFavourite, ListPreference, RegisterView, LoginView, UserView, LogoutView 
+from .views import AddFavourite, AddCategoryPreference, AddTypePlacePreference, ListFavourite, ListPreference, RegisterView, LoginView, UpdateCategoryPreference, UserView, LogoutView 
 
 urlpatterns = [
     path('users/register/', RegisterView.as_view()),
@@ -11,5 +11,6 @@ urlpatterns = [
     path('users/preference/category/create/', AddCategoryPreference.as_view()),
     path('users/preference/typeplace/create/', AddTypePlacePreference.as_view()),
     path('users/favourites/', ListFavourite.as_view()),
-    path('users/preferences/', ListPreference.as_view())
+    path('users/preferences/', ListPreference.as_view()),
+    path('users/preference/category/update', UpdateCategoryPreference.as_view())
 ]
