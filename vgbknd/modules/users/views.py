62638536a416175ecae3a9dd71977beacb87eb81
Sebastian <jsebastpz@gmail.com>
2021-07-01 22:09:35 -0500

    vguide-116: comments and ranking

--- vgbknd/modules/users/views.py
+++ vgbknd/modules/users/views.py
@@ -1,4 +1,3 @@
-from vgbknd.modules.places.models import Department
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
